e17a8a316331e662ed6bffdaedda74acc1276939
David-Ubuntu <randjelovic.david01@gmail.com>
2024-10-30 11:51:17 +0100

    Exy deployment

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/exy')));
+app.use(express.static(path.join(__dirname, 'dist/exy/browser')));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/exy' }),
+  res.sendFile('index.html', { root: 'dist/exy/browser' }),
 );
 
 const PORT = process.env.PORT || 3000;
